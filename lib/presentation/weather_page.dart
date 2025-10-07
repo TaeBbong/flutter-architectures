8b13be01d994bda74f42272442f301debc670e38
TaeBbong <mok05289@korea.ac.kr>
2025-10-07 20:49:14 +0900

    feat: implement page ui(weather)

---
+++ lib/presentation/weather_page.dart
@@ -0,0 +1,20 @@
+// presentation/weather_page.dart
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../data/weather_repository.dart';
+
+class WeatherPage extends ConsumerWidget {
+  const WeatherPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // note 
+    final weatherAsync = ref.watch(fetchWeatherProvider('London'));
+    return weatherAsync.when(
+      data: (weather) => Text(weather.temp.toString()),
+      loading: () => const Center(child: CircularProgressIndicator()),
+      error: (e, _) => Text(e.toString()),
+    );
+  }
+}
